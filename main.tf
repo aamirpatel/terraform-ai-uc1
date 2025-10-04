50c6f1c503a437d949c60f132f2ee68e536c0a3f
Aamir Patel <paamir89@gmail.com>
2025-10-04 08:34:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "logs" {
-  bucket = "mybucket71198"
-  acl    = "private"
+#resource "aws_s3_bucket" "logs" {
+#  bucket = "mybucket71198"
+#  acl    = "private"
+#}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
+
